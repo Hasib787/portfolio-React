30352cae964129a5f334dbfe5bb96fc4ff139682
Md. Masud Rana <mr.hasib45@gmail.com>
2022-01-27 23:38:27 +0600

    edit contact component

--- src/views/all-home-version/HomeDark.jsx
+++ src/views/all-home-version/HomeDark.jsx
@@ -56,7 +56,7 @@ const HomeDark = () => {
           <PhotoGallery />
         </div>
       </div>
-      {/* End Portfolio */}
+      {/* End end Photo gallery */}
 
       <div className="edina_tm_testimonials" id="quotes">
         <div className="container">
@@ -104,36 +104,6 @@ const HomeDark = () => {
             <Address />
           </div>
           {/* End Address Info */}
-          <div className="mainpart">
-            <div
-              className="left"
-              data-aos="fade-right"
-              data-aos-duration="1200"
-              data-aos-delay="300"
-            >
-              <div className="title">
-                <p>
-                  I'm always open to discussing product
-                  <br />
-                  <span> design work or partnerships.</span>
-                </p>
-              </div>
-              {/* End title */}
-              <div className="fields">
-                <Contact />
-              </div>
-              {/* End Contact Form */}
-            </div>
-            <div
-              className="right"
-              data-aos="fade-right"
-              data-aos-duration="1200"
-              data-aos-delay="400"
-            >
-              <Map />
-            </div>
-            {/* End Map */}
-          </div>
         </div>
       </div>
       {/* /CONTACT */}
